a2c3cbd1b596c8e4e92c9df0a6f04a5e3370cc15
Genki Hagata <ghagata@hugeinc.com>
2016-11-02 18:39:14 -0700

    🤗  Adds handlebars templates & root route

--- Day-2-Hapi/server.js
+++ Day-2-Hapi/server.js
@@ -1,17 +1,33 @@
 'use-strict';
 
 const Hapi = require('hapi');
+const Inert = require('inert');
+const Vision = require('vision');
+const Handlebars = require('handlebars');
 
 const server = new Hapi.Server();
 server.connection({
     port: 3000
 });
 
+//Register Plugins
+server.register(Inert, () => {});
+server.register(Vision, err => {});
+
+//Setup views
+server.views({
+    engines: {
+        html: Handlebars
+    },
+    relativeTo: __dirname,
+    path: 'templates'
+})
+
 server.route({
     path: '/',
     method: 'GET',
     handler: function(request, reply) {
-        reply('Hello, Hapi World!')
+        reply.view('index', {msg: "Hello, Hapi World"})
     }
 })
 
