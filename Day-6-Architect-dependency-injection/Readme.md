046498d8e706e682a585cb2a4a4d27c3703ebbd8
hagata <genki@hagata.info>
2016-11-06 22:02:03 -0800

    💎 Adds the basis for db fetched plugin loading

---
+++ Day-6-Architect-dependency-injection/Readme.md
@@ -0,0 +1,3 @@
+Still could use:
+- Ability to add/extend routes through plugins
+- Better way of handling global core modules, e.g., core/views
--- Day-6-Architect-dependency-injection/config.architect.js
+++ Day-6-Architect-dependency-injection/config.architect.js
@@ -1,4 +1,21 @@
-module.exports = [
-    './plugins/hello',
-    './core/server'
+'use strict'
+//Get list of enabled plug-ins from some db
+
+const plugins = []
+const pluginDb = [
+    "hello",
+    // "admin"
 ]
+
+/**
+ * Set plugin/ paths to include all enabled plugins
+ */
+for (const entry of pluginDb){
+    plugins.push(`./plugins/${entry}`)
+}
+plugins.push(`./core/server`)
+
+/**
+ * Export compiled config list of enabled plugins.
+ */
+module.exports = plugins;
