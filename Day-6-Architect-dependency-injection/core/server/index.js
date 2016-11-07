32a6f72d7c8b6baaa82632bf991c5bac91c00b25
hagata <genki@hagata.info>
2016-11-06 17:30:30 -0800

    💎 Adds basic Koa routes

--- Day-6-Architect-dependency-injection/core/server/index.js
+++ Day-6-Architect-dependency-injection/core/server/index.js
@@ -1,5 +1,14 @@
 'use strict'
+const koa = require('koa');
+const route = require('koa-route');
+const app = koa();
 
 module.exports = function(options, imports, register){
-    console.log("Server Loaded")
+
+    app.use(route.get('/', function* (){
+        this.body = "Hello, World! - From the Koa server."
+    }))
+
+    console.log(`Server listening on port:8000`)
+    app.listen(8000);
 }
