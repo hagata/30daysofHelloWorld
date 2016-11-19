61887143ea38ddbe9166d9725fabcfe40b724a4d
Genki Hagata <ghagata@hugeinc.com>
2016-11-19 12:32:48 -0500

    Cleans up socket.io implementation

--- Day-15-Socket-io/app.js
+++ Day-15-Socket-io/app.js
@@ -12,21 +12,23 @@ const view = views('./views', {
     }
 });
 
+// Setup app handlers
 const Handlers = {
     root: function*(){
         this.body = yield view('index')
     }
 }
 
+// Route setup
 app.use(route.get('/', Handlers.root));
 app.use(staticRoute('./'));
-io.on('connection', () => {
-console.log('io connection')
+
+// Socket IO
+io.on('connection', (socket) => {
+    console.log('io connection connected?', socket.connected);
+    console.log('a user connected');
 })
 
-io.on('connection', function(socket){
-  console.log('a user connected');
-});
-
+// Start the server.
 server.listen(3000);
 console.log('listening on port:3000')
---
+++ Day-15-Socket-io/js/app.js
@@ -0,0 +1,3 @@
+// emmits 'connected'.
+console.log('%cApp Loaded', 'font-style:bold; font-size: 16px; color: #00aa11')
+const socket = io();
