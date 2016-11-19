4998e383fe15be9d2f9375aed8d6a39016a2cfaf
Genki Hagata <ghagata@hugeinc.com>
2016-11-19 13:09:50 -0500

    🔌 Adds socket.IO events!

--- Day-15-Socket-io/js/app.js
+++ Day-15-Socket-io/js/app.js
@@ -1,3 +1,39 @@
 // emmits 'connected'.
-console.log('%cApp Loaded', 'font-style:bold; font-size: 16px; color: #00aa11')
+console.log('%cApp Loaded', 'font-style:bold; font-size: 16px; color: #00aa11');
 const socket = io();
+
+// Init app
+// global events
+socket.on('controller.pressed.main', e => {
+  console.log('controller passed', e);
+});
+
+let messageForm = document.querySelector('.message-form') || null;
+if (messageForm) {
+  let messageInput = document.querySelector('.message-input');
+
+  console.log('message', messageInput);
+
+  messageForm.addEventListener('submit', e => {
+    let msg = messageInput.value;
+    e.preventDefault();
+
+    // Emit the socket.io event with the message data
+    socket.emit('chatMessage', msg);
+    messageInput.value = '';
+  });
+}
+
+// load modules on a page
+
+let controller = document.querySelector('.controller') || null;
+if (controller) {
+  console.log('controller started');
+
+  let controllerBtn = document.querySelector('.controller-button');
+  controllerBtn.addEventListener('click', e => {
+    e.preventDefault();
+    console.log('controller button clicked');
+    socket.emit('controller.pressed.main', e);
+  });
+}
