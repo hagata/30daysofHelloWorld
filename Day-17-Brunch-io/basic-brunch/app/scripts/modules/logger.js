2ba8c6e53328723c2fbd46ba6221000532af16d0
hagata <genki@hagata.info>
2016-11-20 18:46:55 -0800

    Adds timestamp to recursive logger

--- Day-17-Brunch-io/basic-brunch/app/scripts/modules/logger.js
+++ Day-17-Brunch-io/basic-brunch/app/scripts/modules/logger.js
@@ -3,7 +3,7 @@ import random from './random';
 console.log('random is',random)
 export default function logs(phrase) {
     setTimeout(function() {
-        console.log(phrase);
+        console.log(`time: ${Date.now()}: ${phrase}`);
         logs(phrase);
     }, random(1000,3000));
     console.log('logger')
