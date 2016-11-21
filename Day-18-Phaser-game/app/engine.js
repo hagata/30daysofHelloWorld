fd9ceb979b79ea4728194d18ea28ac85e80e7488
hagata <genki@hagata.info>
2016-11-20 19:56:46 -0800

    Adds basic Phaser setup

--- Day-18-Phaser-game/app/engine.js
+++ Day-18-Phaser-game/app/engine.js
@@ -1,8 +1,33 @@
+// Phaser requires global objects. define here.
 window.PIXI = require('phaser/build/custom/pixi')
 window.p2 = require('phaser/build/custom/p2')
 window.Phaser = require('phaser');
-document.addEventListener('DOMContentLoaded', () => {
-  // do your setup here
-  console.log('phaser',Phaser);
-  console.log('Initialized app');
-});
+
+console.log('phaser', Phaser);
+
+
+/**
+ * Phaser.Game(height, width, renderer, domElementID, object)
+ * Renderer is either, Phaser.Canvas, Phaser.WebGL, Phaser.AUTO
+ */
+const game = new Phaser.Game(800, 600, Phaser.AUTO, '', {
+  preload,
+  create,
+  update
+})
+
+function preload() {
+
+}
+
+function create() {
+
+}
+
+function update() {
+
+}
+
+
+console.log('%cPhaser Loaded', 'font-size:14px; color: #2ece00');
+
