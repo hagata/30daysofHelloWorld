fa9db7dc7b9389d6bbb4712d8ca223805170e3c5
hagata <genki@hagata.info>
2016-11-20 19:43:58 -0800

    Adds Phaser Base with Brunch setup

---
+++ Day-18-Phaser-game/app/engine.js
@@ -0,0 +1,8 @@
+window.PIXI = require('phaser/build/custom/pixi')
+window.p2 = require('phaser/build/custom/p2')
+window.Phaser = require('phaser');
+document.addEventListener('DOMContentLoaded', () => {
+  // do your setup here
+  console.log('phaser',Phaser);
+  console.log('Initialized app');
+});
