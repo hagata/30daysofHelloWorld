d7e7950bf9f5ed9ec634def7e190e83e0d601460
hagata <genki@hagata.info>
2016-11-26 18:56:45 -0800

    Adds private tasks & security

--- Day-23-Meteor/basic-app/imports/api/tasks.js
+++ Day-23-Meteor/basic-app/imports/api/tasks.js
@@ -7,7 +7,12 @@ export const Tasks = new Mongo.Collection('tasks');
 
 if (Meteor.isServer) {
     Meteor.publish('tasks', function tasksPublication() {
-        return Tasks.find();
+        return Tasks.find({
+            $or: [
+                {private: {$ne: true}},
+                {owner: this.userId},
+            ]
+        });
     });
 }
 
@@ -30,12 +35,35 @@ Meteor.methods({
   'tasks.remove'(taskId) {
     check(taskId, String);
 
+    const task = Tasks.findOne(taskId);
+    if(task.private && task.owner !== this.userId) {
+        throw new Meteor.Error('not-authorized');
+    }
+
     Tasks.remove(taskId);
   },
   'tasks.setChecked'(taskId, setChecked) {
     check(taskId, String);
     check(setChecked, Boolean);
 
+    const task = Tasks.findOne(taskId);
+    if(task.private && task.owner !== this.userId) {
+        throw new Meteor.Error('not-authorized');
+    }
+
     Tasks.update(taskId, { $set: { checked: setChecked } });
   },
+  'tasks.setPrivate'(taskId, setTotPrivate) {
+      check(taskId, String);
+      check(setTotPrivate, Boolean);
+
+      const task = Tasks.findOne(taskId);
+
+      if (task.owner !== this.userId) {
+          throw new Meteor.Error('not-authorized');
+      }
+
+      Tasks.update(taskId, {$set:{private:setTotPrivate}});
+
+  }
 });
--- Day-23-Meteor/basic-app/imports/ui/task.js
+++ Day-23-Meteor/basic-app/imports/ui/task.js
@@ -4,6 +4,12 @@ import {Tasks} from '../api/tasks.js';
 
 import './task.html';
 
+Template.task.helpers({
+  isOwner() {
+    return this.owner === Meteor.userId();
+  }
+})
+
 Template.task.events({
   'click .toggle-checked'() {
     // Set the checked property to the opposite of its current value
@@ -12,4 +18,7 @@ Template.task.events({
   'click .delete'() {
     Meteor.call('tasks.remove', this._id);
   },
+  'click .toggle-private'() {
+    Meteor.call('tasks.setPrivate', this._id, !this.private);
+  }
 });
