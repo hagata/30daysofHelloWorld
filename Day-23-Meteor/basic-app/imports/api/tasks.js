e7634c1b73ceab79ca60cf319e767d2b7a6740c3
hagata <genki@hagata.info>
2016-11-26 18:46:53 -0800

    Adds pub/sub secure layer to tasks DB

--- Day-23-Meteor/basic-app/imports/api/tasks.js
+++ Day-23-Meteor/basic-app/imports/api/tasks.js
@@ -5,6 +5,12 @@ import { check } from 'meteor/check';
 
 export const Tasks = new Mongo.Collection('tasks');
 
+if (Meteor.isServer) {
+    Meteor.publish('tasks', function tasksPublication() {
+        return Tasks.find();
+    });
+}
+
 Meteor.methods({
   'tasks.insert'(text) {
     check(text, String);
