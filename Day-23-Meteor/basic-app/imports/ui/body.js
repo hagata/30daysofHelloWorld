fc6118adf91c61185f771fd2ac87a8f1a09aa87f
hagata <genki@hagata.info>
2016-11-26 18:17:50 -0800

    Adds more complete Todo function + Users

--- Day-23-Meteor/basic-app/imports/ui/body.js
+++ Day-23-Meteor/basic-app/imports/ui/body.js
@@ -1,12 +1,30 @@
+import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
+import { ReactiveDict } from 'meteor/reactive-dict';
+
 import { Tasks } from '../api/tasks';
 
 import './task.js';
 import './body.html';
 
+Template.body.onCreated(function bodyOnCreated() {
+    this.state = new ReactiveDict();
+});
+
 Template.body.helpers({
     tasks() {
-        return Tasks.find({}, {sort: {createdAt: -1}});
+        const instance = Template.instance();
+        console.log('instance is', instance.state.get('hideCompleted'))
+        if (instance.state.get('hideCompleted')) {
+            console.log('Hide Complete')
+            // If hide completed is checked, filter tasks
+            return Tasks.find({ checked: { $ne: true } }, { sort: { createdAt: -1 } });
+        }
+        // Otherwise, return all of the tasks
+        return Tasks.find({}, { sort: { createdAt: -1 } });
+    },
+    incompleteCount() {
+        return Tasks.find({ checked: { $ne: true } }).count();
     }
 });
 
@@ -21,10 +39,17 @@ Template.body.events({
         // insert value to Task db
         Tasks.insert({
             text,
-            createdAt: new Date()
+            createdAt: new Date(),
+            owner: Meteor.userId(),
+            username: Meteor.user().username,
+
         })
 
         // clear form
         target.text.value = '';
-    }
+    },
+    'change .hide-completed input'(event, instance) {
+        console.log('Change hide state')
+        instance.state.set('hideCompleted', event.target.checked);
+    },
 })
