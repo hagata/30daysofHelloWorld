f36ff076c63e8adfd54a059df777281231d33609
Genki Hagata <ghagata@hugeinc.com>
2016-11-29 11:44:47 -0800

    Successfully caches Map data for offline 🙌

--- Day-26-Service-Worker/sw-map-br/app/assets/sw.js
+++ Day-26-Service-Worker/sw-map-br/app/assets/sw.js
@@ -1,46 +1,47 @@
 
-    self.addEventListener('install', (e) => {
-        console.log('SERVICEWORKER Installed')
-        e.waitUntil(
+    self.addEventListener('install', e => {
+      console.log('SERVICEWORKER Installed');
+      e.waitUntil(
             caches.open('base').then(cache => {
-                return cache.addAll([
-                    '/',
-                    'index.html',
-                    'global.css',
-                    'app.js',
-                    'vendors.js',
-                    'sw.js'
-    //                 '/index.html?homescreen=1',
-    //                 '/?homescreen=1',
-    //                 '/styles/main.css',
-    //                 '/scripts/main.min.js',
-    //                 '/sounds/airhorn.mp3'
-                ])
-            })
-        )
-    })
-
-    self.addEventListener('fetch', (e) => {
-        e.respondWith(
-            caches.match(e.request).then(response => {
-                return response || fetch(e.request);
+              return cache.addAll([
+                '/',
+                'index.html',
+                'global.css',
+                'app.js',
+                'vendors.js',
+                'sw.js',
+                'images/offline.png',
+                'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700'
+              ]);
             })
         );
+    });
 
+    self.addEventListener('fetch', e => {
+        // Check for google maps api related content requests.
+      if (e.request.url.indexOf('https://maps.googleapis.com') === 0 ||
+          e.request.url.indexOf('https://csi.gstatic.com') === 0 ||
+          e.request.url.indexOf('https://maps.gstatic.com/') === 0) {
+        console.log('%cMaps Request', 'color:green;', e.request.url);
 
-        //Check for google maps api related content requests.
-        if (e.request.url.indexOf('https://maps.googleapis.com') === 0 ||
-            e.request.url.indexOf('https://csi.gstatic.com' === 0)) {
-            console.log('%cMaps Request', 'color:green;', e.request.url)
-            // event.respondWith(
-                // Handle Maps API requests in a generic fashion,
-                // by returning a Promise that resolves to a Response.
-            // );
-        } else {
-            console.log('Request:', e.request.url)
-            // event.respondWith(
-                // Some default handling.
-            // );
-        }
-        // )
-    })
+        e.respondWith(
+              caches.match(e.request).then(function(resp) {
+                return resp || fetch(e.request).then(function(response) {
+                  caches.open('maps').then(function(cache) {
+                    cache.put(e.request, response.clone());
+                  });
+                  return response.clone();
+                });
+              }).catch(function() {
+                return caches.match('/images/offline.png');
+              })
+            );
+      } else {
+        console.log('Request:', e.request.url);
+        e.respondWith(
+              caches.match(e.request).then(response => {
+                return response || fetch(e.request);
+              })
+            );
+      }
+    });
