3d8528a59029e93ae306d411aed54c50c4acd490
Genki Hagata <ghagata@hugeinc.com>
2016-11-28 19:37:04 -0800

    adds check for serviceworker

--- Day-26-Service-Worker/basic-sw/pages/index.js
+++ Day-26-Service-Worker/basic-sw/pages/index.js
@@ -1,4 +1,24 @@
 import React from 'react'
-export default () => (
-  <div>Welcome to next.js!</div>
+import SW from '../utils/serviceworker';
+
+export default class extends React.Component {
+    constructor() {
+        super();
+
+        this.init();
+    }
+
+    render() {
+        return(<div className="hello">Hello, Next JS!</div>
 )
+    }
+    init() {
+        //check for SW
+        console.log('SW?', ('serviceWorker' in navigator))
+        if ('serviceWorker' in navigator) {
+            // load ServiceWorker
+            SW();
+        }
+        this.render()
+    }
+}
