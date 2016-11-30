e2bead2920bd0389291060bf7fef283e23bd28b7
hagata <genki@hagata.info>
2016-11-29 20:25:53 -0800

    adds functioning tableViewCells

---
+++ Day-27-Swift-3/sw-maptest/sw-maptest/tableCell.swift
@@ -0,0 +1,14 @@
+//
+//  tableviewCellTableViewCell.swift
+//  sw-maptest
+//
+//  Created by Genki on 11/29/16.
+//  Copyright © 2016 Genki Hagata. All rights reserved.
+//
+
+import UIKit
+
+class tableCell: UITableViewCell {
+
+    
+}
--- Day-27-Swift-3/sw-maptest/sw-maptest/tableViewController.swift
+++ Day-27-Swift-3/sw-maptest/sw-maptest/tableViewController.swift
@@ -9,6 +9,36 @@
 import Foundation
 import UIKit
 
+
+
 class TableViewController: UITableViewController {
+
     
+    var names = [String]()
+    
+      override func viewDidLoad() {
+        names = ["Marin Headlands", "Redwoods"]
+    }
+    
+     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return names.count
+        
+    }
+    
+     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        print(names[indexPath.row])
+
+        // Set cell with custom nib (xib)
+                let cell = Bundle.main.loadNibNamed("TableViewCell", owner: self, options: nil)?.first as! TableViewCell
+        cell.titleLabel.text = names[indexPath.row]
+        return cell
+
+// Set cell with reuseable cell
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "tableCell")
+//        cell?.textLabel?.text = names[indexPath.row]
+//        return cell!
+    }
+    
+
 }
